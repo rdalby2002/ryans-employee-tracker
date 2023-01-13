2987b4d9e74b63e1d2eb43b66cd4dde7e756ca7f
rdalby2002 <ryancdalby@gmail.com>
2023-01-12 18:30:07 -0600

    Added role functions for each possible response to init prompt. Still dealing with an unidentified error

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
-// Connect to database
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    // MySQL username,
     user: 'root',
-    // MySQL password
     password: 'root',
     database: 'employee_db'
   },
@@ -33,7 +30,6 @@ const init = () => {
         ]
       }
     ]).then(ans => {
-      // console.log(ans.initialize);
       switch (ans.initialize) {
         case "View all departments": viewDept();
           break;
@@ -58,3 +54,119 @@ const init = () => {
 
 init();
 
+const viewDept = () => {
+  db.query(`SELECT * FROM department`, (err, results) => {
+    err ? console.error(err) : console.table(results);
+  }).then(function(){init()});
+};
+
+const viewRoles = () => {
+  db.query(`SELECT * FROM roles`, (err, results) => {
+    err ? console.error(err) : console.table(results);
+  }).then(function(){intro()});
+};
+
+const viewEmployees = () => {
+  db.query(`SELECT * FROM employees`, (err, results) => {
+    err ? console.error(err) : console.table(results);
+  }).then(function(){intro()});
+};
+
+const addDept = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the name of the department you'd like to add?",
+        name: "addDept"
+      }
+    ]).then(ans => {
+      db.query(`INSERT INTO department(name)
+                    VALUES(?)`, ans.addDept, (err, results) => {
+        if (err) {
+          console.log(err)
+        } else {
+          db.query(`SELECT * FROM department`, (err, results) => {
+            err ? console.error(err) : console.table(results);
+          })
+        }
+      }
+      )
+    }).then(function(){intro()});
+};
+
+const addRole = () => {
+  const deptChoices = () => db.promise().query(`SELECT * FROM department`)
+    .then((rows) => {
+      let names = rows[0].map(obj => obj.name);
+      return names
+    })
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the title of the role you'd like to add?",
+        name: "roleTitle"
+      },
+      {
+        type: "input",
+        message: "What is the salary for this role?",
+        name: "roleSalary"
+      },
+      {
+        type: "list",
+        message: "Which department is this role in?",
+        name: "addDept",
+        choices: deptChoices
+      }
+    ]).then(ans => {
+      db.promise().query(`SELECT id FROM department WHERE name = ?`, ans.addDept)
+        .then(answer => {
+          let mappedId = answer[0].map(obj => obj.id);
+          return mappedId[0]
+        })
+        .then((mappedId) => {
+          db.promise().query(`INSERT INTO roles(title, salary, department_id)
+                VALUES(?, ?, ?)`, [ans.roleTitle, ans.roleSalary, mappedId]);
+        })
+    }).then(function(){intro()});
+};
+
+const addEmployee = () => {
+  const rollChoices = () => db.promise().query(`SELECT * FROM roles`)
+  .then((rows) => {
+      let names = rows[0].map(obj => obj.name);
+      return names
+  })
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the employee's first name?",
+        name: "firstName"
+      },
+      {
+        type: "input",
+        message: "What is the employee's last name?",
+        name: "lastName"
+      },
+      {
+        type: "list",
+        message: "What is the employee's role?",
+        name: "employeeRole",
+        choices: rollChoices
+      }
+    ]).then(ans => {
+      db.query(`INSERT INTO employees(first_name, last_name)
+                    VALUES(?, ?)`, [ans.firstName, ans.lastName], (err, results) => {
+        if (err) {
+          console.log(err)
+        } else {
+          db.query(`SELECT * FROM employees`, (err, results) => {
+            err ? console.error(err) : console.table(results);
+          })
+        }
+      }
+      )
+    }).then(function(){intro()});
+};
